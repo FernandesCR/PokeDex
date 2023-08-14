c26854b36af07493df1a973376cfe090a8b205bf
Fernandes <matheusramosramos53@gmail.com>
2023-08-14 17:30:42 -0300

    .

--- paginas/normal/script.js
+++ paginas/normal/script.js
@@ -1,79 +1,47 @@
-const colors = {
-    grass: '#63BB5B',
-    fire: '#FF9C54',
-    electric: '#ffd500',
-    water: '#4D90D5',
-    ground: '#ffc388',
-    rock: '#C7B78B',
-    fairy: '#EC8FE6',
-    poison: '#AB6AC8',
-    bug: '#90C12C',
-    dragon: '#7766EE',
-    psychic: '#F97176',
-    flying: '#92AADE',
-    fighting: '#CE4069',
-    normal: '#969ea5',
-    ghost: '#5269AC',
-    steel: '#5A8EA1',
-    dark: '#5A5366',
-    ice: '#74CEC0',
-};
+const pokemonImage = document.querySelector('.pokemonImage');
+const namePokemon = document.querySelector('.namePokemon')
+const typePokemon = document.querySelector('.typePokemon')
+const idPokemon = document.querySelector('.idPokemon')
 
-let dadosAtualSave = null;
-
-const pokemonFetch = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+const pokemonFetch = async (pokemonType) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/type/${pokemonType}`);
     const dados = await APIResponse.json();
-    return dados;
+    return dados.pokemon.map(poke => poke.pokemon.name);
 };
 
-const renderPokemon = async (pokemon) => {
-    const dados = await pokemonFetch(pokemon);
-    const cor2 = document.getElementById('cor2');
-    const cor = document.getElementById('cor');
-    const container = document.getElementById('container');
+const renderPokemon = async (pokemonType) => {
+    const pokemonNames = await pokemonFetch(pokemonType);
 
-    if (dados) {
-        const pokemonTipos = dados.types.map(type => type.type.name);
-        for (const type of pokemonTipos) {
-            if (colors[type]) {
-                container.style.border = `2px solid ${colors[type]}`;
-                cor.style.border = `2px solid ${colors[type]}`;
-                cor2.style.backgroundColor = colors[type];
-                break;
-            }
-        }
+    const pokeCardContainer = document.getElementById('pokeCardContainer');
 
-        dadosAtualSave = dados;
+    for (const name of pokemonNames) {
+        const pokemonInfoResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const pokemonInfo = await pokemonInfoResponse.json();
 
-        pokemonImage.src = dados.sprites.other['official-artwork'].front_default;
+        const pokeCard = document.createElement('div');
+        pokeCard.className = 'pokeCard';
 
-        pokemonName.forEach(name => {
-            name.textContent = dados.name.charAt(0).toUpperCase() + dados.name.slice(1).toLowerCase();
-        });
-        pokemonNumber.forEach(id => {
-            id.textContent = '' + dados.id;
-        });
-        renderTypeImages(pokemonTipos);
-        renderTypeImagesNames(pokemonTipos);
-        renderTypeOrTypes(pokemonTipos);
+        const pokemonImage = document.createElement('img');
+        pokemonImage.src = pokemonInfo.sprites.other['official-artwork'].front_default;
+        pokemonImage.style.width = '100px';
+        pokemonImage.style.height = '100px';
+
+        const idPokemon = document.createElement('div');
+        idPokemon.className = 'idPokemon';
+        idPokemon.textContent = pokemonInfo.id;
+
+        const namePokemon = document.createElement('div');
+        namePokemon.className = 'namePokemon';
+        namePokemon.textContent = pokemonInfo.name.charAt(0).toUpperCase() + pokemonInfo.name.slice(1).toLowerCase();
+        
+        pokeCard.appendChild(pokemonImage);
+        pokeCard.appendChild(idPokemon);
+        pokeCard.appendChild(namePokemon);
+        
+        pokeCardContainer.appendChild(pokeCard);
+        
     }
-
-    const renderShiny = () => {
-        if (dadosAtualSave) {
-            pokemonImage.src = dadosAtualSave.sprites.other['official-artwork'].front_shiny;
-        }
-    };
-
-    btnShiny.addEventListener('click', () => {
-        renderShiny();
-    });
-
-    const renderNormal = () => {
-        if (dadosAtualSave)
-            pokemonImage.src = dados.sprites.other['official-artwork'].front_default;
-    };
-    btnNormal.addEventListener('click', () => {
-        renderNormal();
-    });
 };
+
+renderPokemon('normal');
+
